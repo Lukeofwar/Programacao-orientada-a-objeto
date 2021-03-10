6329c655d06cecba43d2ecc56864f98b0a79fb16
Lucas <matheuslucaskid@gmail.com>
2021-03-10 19:36:54 -0300

    criei uma classe chamada caneta

---
+++ OlaMundo/src/olamundo/OlaMundo.java
@@ -0,0 +1,10 @@
+package olamundo;
+
+public class OlaMundo {
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello<< olá mundo >>world!");
+        System.out.println("❤❤❤");
+               
+    }
+  }
